2917d6dc32607ef8bbd1ef8199f7020a645a5583
Kamal Varma Kambe <kamal.kambe@rockyou.com>
2017-09-22 10:55:04 +0530

    Fixed the vim indentation issue

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-	def index
-		@user = User.find(params[:id])
-	end
+  def index
+    @user = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+    @profile = @user.profile
+  end
 end
